4eaef6f901cba7109c2e3655463f6663f73cdd1b
Zain Padela <zpadela@gmail.com>
2021-12-15 11:22:12 -0500

    Styling changes to recipe and shopping component

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -9,7 +9,8 @@ export class RecipeListComponent implements OnInit {
   recipes: Recipe[] = [
     new Recipe('A test recipe', 'This is a test', 'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&webp=true&resize=600,545')
   ];
-  constructor() { }
+
+  constructor() {}
 
   ngOnInit(): void {
   }
